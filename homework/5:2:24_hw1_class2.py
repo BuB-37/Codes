65cd293010f3b689e4203eecbc77a677d9748f82
BuB-37 <aadhees.j@gmail.com>
2024-09-10 21:19:40 -0700

    Add files via upload

---
+++ homework/5:2:24_hw1_class2.py
@@ -0,0 +1,5 @@
+#print function with above all clubbed names in it separated by one of the escape characters
+names = ["Alexander","Victoria","Pearl","Braganza","Abell","Neo","Niko","Crystal","Derik","Matt","Abby","Adam","Ozzy","Stella"]
+for name in names:
+    print(name)
+print(" ")
---
+++ homework/5:9:24_hw2_class3.py
@@ -0,0 +1,5 @@
+age = int(input("Lets see if you can ride this roller coaster. Enter your age please :"))
+if (age >= 10):
+    print("You can ride this roller coaster!!!!")
+else:
+    print("You can't ride this roller coaster yet. Come back when you are older.")
