5ddb946bb7906f65c98c53f2b017863f70ad31d7
BuB-37 <aadhees.j@gmail.com>
2024-09-11 21:01:59 -0700

    Add files via upload

---
+++ homework/6-27-24_hw_Month_Numbers.py
@@ -0,0 +1,75 @@
+#MONTH NUMBERS
+print("---- MONTH NUMBERS ----")
+print("")
+print("1 - JANUARY")
+print("")
+print("2 - FEBUARY")
+print("")
+print("3 - MARCH")
+print("")
+print("4 - APRIL")
+print("")
+print("5 - MAY")
+print("")
+print("6 - JUNE")
+print("")
+print("7 - JULY")
+print("")
+print("8 - AUGUST")
+print("")
+print("9 - September")
+print("")
+print("10 - October")
+print("")
+print("11 - November")
+print("")
+print("12 - December")
+print("----------------------")
+print("")
+
+#INPUT
+month_num = int(input("ENTER THE THE CORESPONDING NUMBER FOR WHICH MONTH YOU WANT TO CHOSE : "))
+
+#IF ELSE 
+if month_num == 1:
+    print("")
+    print("January has 31 Days")
+elif month_num == 2:
+    print("")
+    print("Febuary has either 29 or 28 Days")
+elif month_num == 3:
+    print("")
+    print("March has 31 Days")
+elif month_num == 4:
+    print("")
+    print("April has 30 Days")
+elif month_num == 4:
+    print("")
+    print("May has 31 Days")
+elif month_num == 6:
+    print("")
+    print("June has 30 Days")
+elif month_num == 7:
+    print("")
+    print("July has 31 Days")
+elif month_num == 8:
+    print("")
+    print("August has 31 Days")
+elif month_num == 9:
+    print("")
+    print("September has 30 Days")
+elif month_num == 10:
+    print("")
+    print("October has 31 Days")
+elif month_num == 11:
+    print("")
+    print("November has 30 Days")
+elif month_num == 12:
+    print("")
+    print("December has 31 Days")
+else:
+    print("")
+    print("Enter a valid number")
+
+print("")
+print("----------------------")
