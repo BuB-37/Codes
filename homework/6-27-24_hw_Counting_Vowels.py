8814a5946b96546ba0aa7c4434c16db3d3ea7e35
BuB-37 <aadhees.j@gmail.com>
2024-09-11 21:04:59 -0700

    Add files via upload

---
+++ homework/6-27-24_hw_Counting_Vowels.py
@@ -0,0 +1,12 @@
+# Define the vowels
+vowel = "aeiouAEIOU"
+
+#Take inpout from the user
+word = input("ENTER A WORD TO FIND OUT HOW MANY VOWELS IT HAS : ")
+ 
+#Use a list comprehension to check each character in the word
+# Sum up 1 for each vowel found in the word
+num_vowels = sum(1 for char in word if char in vowel)
+
+#Print the result
+print(f"The number of vowels in '{word}' is {num_vowels}")
